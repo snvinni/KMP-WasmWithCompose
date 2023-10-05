0e2747f19cc7a4d2617c6d9318eff1f4a27979fb
vini <vini.snascimento00@gmail.com>
2023-10-05 16:15:13 -0300

    feat: update project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 
-rootProject.name = "RickAndMortyKMP"
+rootProject.name = "KMP-WasmWithCompose"
 
 include(":shared")
 include(":web")
